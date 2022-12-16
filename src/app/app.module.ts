f278dedbee3cae4967a01f40e154ca6ccbf222ac
abdulmoiez876 <abdul.moiez876@gmail.com>
2022-12-16 17:33:50 +0500

    redirecting and wild cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 import { Routes, RouterModule } from '@angular/router';
 
@@ -23,6 +24,8 @@ const appRoutes: Routes = [
     {path: ':serverId', component: ServerComponent},
     {path: ':serverId/edit', component: EditServerComponent},
   ]},
+  {path: 'notFound', component: NotFoundComponent},
+  {path: '**', redirectTo: '/notFound'}
 ];
 
 @NgModule({
